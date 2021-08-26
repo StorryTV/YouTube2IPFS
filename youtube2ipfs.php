677ce643acf7a16732761955a89ca56a4bd139c6
StorryTV <60582682+StorryTV@users.noreply.github.com>
2021-08-26 18:45:38 +0200

    Update youtube2ipfs.php

--- youtube2ipfs.php
+++ youtube2ipfs.php
@@ -15,63 +15,95 @@ $ipfsapi = array(
 if (filter_var($_GET['video'], FILTER_VALIDATE_URL) && (parse_url($_GET['video'])['host'] === 'www.youtube.com')) {
 	$video = $_GET['video'];
 	$json_ = $_GET['json'];
-	$tmp = explode('.', microtime(true))[0];
-	if (!file_exists(realpath(getcwd()) . '/hashes')) {
-		mkdir(realpath(getcwd()) . '/hashes', 0760);
-	}
-	exec('youtube-dl -f best -q "' . $video . '" -o ' . realpath(getcwd()) . '/videos/' . $tmp . '.tmp 2>&1');
-	if (!file_exists(realpath(getcwd()) . '/hashes')) {
-		mkdir(realpath(getcwd()) . '/hashes', 0760);
-	}
-	exec('curl "' . $ipfsapi['protocol'] . '://' . $ipfsapi['host'] . ':' . $ipfsapi['port'] . '/api/v0/add?stream-channels=true&recursive=false&pin=true&wrap-with-directory=false&progress=false" \
+	$hashes = file_get_contents(realpath(getcwd()) . '/hashes/.ipfs');
+	if (json_decode($hashes, TRUE)[$video]) {
+		$ipfsHash = json_decode($hashes, TRUE)[$video];
+		if ($json_ === 'true') {
+			$arr = array(
+				'Url' => $video,
+				'Hash' => $ipfsHash,
+			);
+			header('Content-type: application/json; charset=utf-8');
+			echo json_encode($arr);
+		} else {
+			header('Content-type: text/html; charset=utf-8');
+			echo $ipfsHash;
+		}
+	} else {
+		$tmp = explode('.', microtime(true))[0];
+		if (!file_exists(realpath(getcwd()) . '/hashes')) {
+			mkdir(realpath(getcwd()) . '/hashes', 0760);
+		}
+		exec('youtube-dl -f best -q "' . $video . '" -o ' . realpath(getcwd()) . '/videos/' . $tmp . '.tmp 2>&1');
+		if (!file_exists(realpath(getcwd()) . '/hashes')) {
+			mkdir(realpath(getcwd()) . '/hashes', 0760);
+		}
+		exec('curl "' . $ipfsapi['protocol'] . '://' . $ipfsapi['host'] . ':' . $ipfsapi['port'] . '/api/v0/add?stream-channels=true&recursive=false&pin=true&wrap-with-directory=false&progress=false" \
 			-X POST \
 			-H "Content-Type: multipart/form-data" \
 			-F file=@"' . realpath(getcwd()) . '/videos/' . $tmp . '.tmp"', $output, $ipfs_upload);
-	$ipfs = json_decode($output[0], TRUE);
-	$arr = array($video => $ipfs['Hash']);
-	file_put_contents(realpath(getcwd()) . '/hashes/.ipfs', json_encode($arr), FILE_APPEND);
-	if ($json_ === 'false') {
-		$arr = array(
-			'Url' => $video,
-			'Hash' => $ipfs['Hash'],
-			'Size' => $ipfs['Size'],
-		);
-		header('Content-type: application/json; charset=utf-8');
-		echo json_encode($arr);
-	} else {
-		header('Content-type: text/html; charset=utf-8');
-		echo $ipfs['Hash'];
+		$ipfs = json_decode($output[0], TRUE);
+		$arr = array($video => $ipfs['Hash']);
+		file_put_contents(realpath(getcwd()) . '/hashes/.ipfs', json_encode($arr), FILE_APPEND);
+		if ($json_ === 'true') {
+			$arr = array(
+				'Url' => $video,
+				'Hash' => $ipfs['Hash'],
+				'Size' => $ipfs['Size'],
+			);
+			header('Content-type: application/json; charset=utf-8');
+			echo json_encode($arr);
+		} else {
+			header('Content-type: text/html; charset=utf-8');
+			echo $ipfs['Hash'];
+		}
 	}
 	unlink(realpath(getcwd()) . '/videos/' . $tmp . '.tmp');
 } elseif (filter_var($_POST['video'], FILTER_VALIDATE_URL) && (parse_url($_POST['video'])['host'] === 'www.youtube.com')) {
 	$video = $_POST['video'];
 	$json_ = $_POST['json'];
-	$tmp = explode('.', microtime(true))[0];
-	if (!file_exists(realpath(getcwd()) . '/hashes')) {
-		mkdir(realpath(getcwd()) . '/hashes', 0760);
-	}
-	exec('youtube-dl -f best -q "' . $video . '" -o ' . realpath(getcwd()) . '/videos/' . $tmp . '.tmp 2>&1');
-	if (!file_exists(realpath(getcwd()) . '/hashes')) {
-		mkdir(realpath(getcwd()) . '/hashes', 0760);
-	}
-	exec('curl "' . $ipfsapi['protocol'] . '://' . $ipfsapi['host'] . ':' . $ipfsapi['port'] . '/api/v0/add?stream-channels=true&recursive=false&pin=true&wrap-with-directory=false&progress=false" \
+	$hashes = file_get_contents(realpath(getcwd()) . '/hashes/.ipfs');
+	if (json_decode($hashes, TRUE)[$video]) {
+		$ipfsHash = json_decode($hashes, TRUE)[$video];
+		if ($json_ === 'true') {
+			$arr = array(
+				'Url' => $video,
+				'Hash' => $ipfsHash,
+			);
+			header('Content-type: application/json; charset=utf-8');
+			echo json_encode($arr);
+		} else {
+			header('Content-type: text/html; charset=utf-8');
+			echo $ipfsHash;
+		}
+	} else {
+		$tmp = explode('.', microtime(true))[0];
+		if (!file_exists(realpath(getcwd()) . '/hashes')) {
+			mkdir(realpath(getcwd()) . '/hashes', 0760);
+		}
+		exec('youtube-dl -f best -q "' . $video . '" -o ' . realpath(getcwd()) . '/videos/' . $tmp . '.tmp 2>&1');
+		if (!file_exists(realpath(getcwd()) . '/hashes')) {
+			mkdir(realpath(getcwd()) . '/hashes', 0760);
+		}
+		exec('curl "' . $ipfsapi['protocol'] . '://' . $ipfsapi['host'] . ':' . $ipfsapi['port'] . '/api/v0/add?stream-channels=true&recursive=false&pin=true&wrap-with-directory=false&progress=false" \
 			-X POST \
 			-H "Content-Type: multipart/form-data" \
 			-F file=@"' . realpath(getcwd()) . '/videos/' . $tmp . '.tmp"', $output, $ipfs_upload);
-	$ipfs = json_decode($output[0], TRUE);
-	$arr = array($video => $ipfs['Hash']);
-	file_put_contents(realpath(getcwd()) . '/hashes/.ipfs', json_encode($arr), FILE_APPEND);
-	if ($json_ === 'false') {
-		$arr = array(
-			'Url' => $video,
-			'Hash' => $ipfs['Hash'],
-			'Size' => $ipfs['Size'],
-		);
-		header('Content-type: application/json; charset=utf-8');
-		echo json_encode($arr);
-	} else {
-		header('Content-type: text/html; charset=utf-8');
-		echo $ipfs['Hash'];
+		$ipfs = json_decode($output[0], TRUE);
+		$arr = array($video => $ipfs['Hash']);
+		file_put_contents(realpath(getcwd()) . '/hashes/.ipfs', json_encode($arr), FILE_APPEND);
+		if ($json_ === 'true') {
+			$arr = array(
+				'Url' => $video,
+				'Hash' => $ipfs['Hash'],
+				'Size' => $ipfs['Size'],
+			);
+			header('Content-type: application/json; charset=utf-8');
+			echo json_encode($arr);
+		} else {
+			header('Content-type: text/html; charset=utf-8');
+			echo $ipfs['Hash'];
+		}
 	}
 	unlink(realpath(getcwd()) . '/videos/' . $tmp . '.tmp');
 } else {
